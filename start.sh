eca643b6b46cb4d240584f70c7b6dbb4016bbeda
Laurent Raharison <raharisonlaurent@gmail.com>
2016-01-10 23:04:19 +0100

    Commentaire script

--- start.sh
+++ start.sh
@@ -5,12 +5,17 @@ chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --user mysql > /dev/null
 
 
+#Demarrage mysql
 /etc/init.d/mysql start
+
+#Load sql
 mysql -u root < script.sql
 
-
+#Copie war file  dans le webapps de tomcat
 cp /webapp.war /var/lib/tomcat7/webapps/webapp.war
 
+#Demarrage tomcat
 service tomcat7 start && tail -f /var/lib/tomcat7/logs/catalina.out
 
+#Ouverture shell
 /bin/bash
